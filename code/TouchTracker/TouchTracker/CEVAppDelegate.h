94250630014a95f8900e3116da27dc4c3274a759
Vikram Aggarwal <vikram@eggwall.com>
2014-07-26 21:53:58 -0700

    Created a Touch Tracker

---
+++ code/TouchTracker/TouchTracker/CEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CEVAppDelegate.h
+//  TouchTracker
+//
+//  Created by Vikram Aggarwal on 7/26/14.
+//  Copyright (c) 2014 Eggwall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ code/TouchTracker/TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Vikram Aggarwal on 7/26/14.
+//  Copyright (c) 2014 Eggwall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CEVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CEVAppDelegate class]));
+    }
+}
